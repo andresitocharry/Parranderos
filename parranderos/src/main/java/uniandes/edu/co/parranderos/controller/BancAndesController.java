e0974d813247f239947db72bd5f8ca8daa2edb50
andresitocharry <a.charryc@uniandes.edu.co>
2024-03-18 08:26:40 -0500

    login siuuu

--- parranderos/src/main/java/uniandes/edu/co/parranderos/controller/BancAndesController.java
+++ parranderos/src/main/java/uniandes/edu/co/parranderos/controller/BancAndesController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.HttpSession;
 import uniandes.edu.co.parranderos.modelo.Usuario;
 import uniandes.edu.co.parranderos.repositorio.UsuarioRepository;
 
@@ -26,19 +27,17 @@ public class BancAndesController {
     public String login(@RequestParam("login") String login,
                     @RequestParam("clave") String clave,
                     @RequestParam("rol") String role,
-                    Model model){
+                    HttpSession session){
         
         // Buscar el usuario en la base de datos por login y contraseña
         
         Usuario usuario = usuarioRepository.encontrarUsuarioConCredenciales(login,clave);
-        System.out.println(login);
-        System.out.println(clave);
-        System.out.println(role);
-        System.out.println();
+
         //si la contraseña o el login no eexisten, la consulta da null entonces vemos que el resultado sea disntinto de null y que el usuario haya puesto bien su rol
         if (usuario != null && usuario.getRol().equals(role)) {
-            // Autenticar al usuario almacenando su información en la sesión
-            model.addAttribute("usuario",usuario);
+            // Autenticar al usuario almacenando su información en una session
+            session.setAttribute("usuario",usuario);
+            session.setAttribute("rol", role);
             
             return "redirect:/home"; // Redirigir a la página de inicio
         } else {
@@ -48,15 +47,15 @@ public class BancAndesController {
         }
     }
 
-    @GetMapping("home")
-    public String home(Model model) {
+    @GetMapping("/home")
+    public String home(HttpSession session, Model model) {
+        
         // Obtener el nombre de usuario
-        Usuario user = (Usuario) model.getAttribute("usuario");
-        String nombreUsuario = user.getNombre();
+        Usuario user = (Usuario) session.getAttribute("usuario");
         
-        // Pasar el nombre de usuario como atributo
-        model.addAttribute("nombreUsuario", nombreUsuario);
-        
+        model.addAttribute("nombreUsuario", user.getNombre());
+        model.addAttribute("rol", session.getAttribute("rol"));
+        System.out.println(model.getAttribute("rol"));
         // Devolver la vista home.jsp
         return "home";
     }
